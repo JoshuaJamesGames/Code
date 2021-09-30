b284541f6cf7b40cb888b2f1c82c183c72a52840
Joshua James <thecoloradothompsons@gmail.com>
2021-09-30 11:47:10 -0600

    Cleaned input string; removing spaces and punctuation.  Added output feedback

--- Palindrome.java
+++ Palindrome.java
@@ -9,7 +9,11 @@ public class Palindrome {
         System.out.println("Enter a phrase for the palindrome test.");
         stringToTest = scnr.nextLine();
 
+        //Formatting for testing
+        stringToTest = stringToTest.toLowerCase();
+        stringToTest = stringToTest.replaceAll("[\\p{Punct}\\s]+", "");
         //Convert to an array in preparation for reversal
+
         char[] stringArray = stringToTest.toCharArray();
         char[] reverseStringArray = new char[stringToTest.length()];
 
@@ -17,8 +21,15 @@ public class Palindrome {
         for(int i=stringArray.length;i>0;i--){
             reverseStringArray[stringArray.length-i]=stringArray[i-1];
         }
+        int isPalindrome = stringToTest.compareTo(String.valueOf(reverseStringArray));
 
         System.out.println("\"" + String.valueOf(reverseStringArray) + "\" is \"" + stringToTest + "\" reversed.");
+        if(isPalindrome ==0){
+            System.out.println("The input is a Palindrome");
+        }
+        else{
+            System.out.println("The input is not a Palindrome");
+        }
         scnr.close();
     }
 
