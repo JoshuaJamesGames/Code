1a4c39f684cce79df6c03cf8901e92219ce18b4c
Joshua James <thecoloradothompsons@gmail.com>
2022-02-23 23:06:46 -0700

    Added more comments

--- ITS320_CTA2_Option2.py
+++ ITS320_CTA2_Option2.py
@@ -10,6 +10,7 @@ from enum import auto
 
 automobile = {} #An empty Dictionary
 #Collect information from the user by prompting and assigning responses to keys
+#Using formatted strings to make the prompts more lively
 automobile['car_brand'] = input("Please enter a car brand. : ")
 automobile['car_model'] = input(f"Please enter the {automobile['car_brand']} model : ")
 automobile['car_year'] = input(f"What year is the {automobile['car_model']}? : ")
@@ -17,6 +18,7 @@ automobile['odometer_start'] = input("What is the starting odometer reading? : "
 automobile['odometer_end'] = input("What is the ending odometer reading? : ")
 automobile['estimated_mpg'] = input(f"Estimated number of miles per gallon for the {automobile['car_brand']} {automobile['car_model']}? : ")
 #Now to print it all out
+#Using 3 lines to keep the length managable and formatted strings so they are easier to read
 print(f"You entered a {automobile['car_year']} {automobile['car_brand']} {automobile['car_model']}.")
 print(f"The vehicle has a starting odometer reading of {automobile['odometer_start']} and an ending odometer reading of {automobile['odometer_end']}.")
 print(f"You believe it gets {automobile['estimated_mpg']} MPG")
