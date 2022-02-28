16861946c67d1daaa6459a59862bb9be9882fb51
Joshua James <thecoloradothompsons@gmail.com>
2022-02-28 15:57:44 -0700

    Week 3 Python Assignment - Inital Commit

---
+++ ITS320_CTA3_Option2.py
@@ -0,0 +1,10 @@
+#Create a program that will calculate the weekly average tax witholding for a customer
+#given the folling weekly income guidelines
+#Income less than/equal to $500: tax rate 10%
+#Income greater than/equal to $1500 and less than $2500: tax rate 20%
+#Income greater than/equal to $2500: tax rate 30%
+
+#Store the income brackets and rates in a dictionary
+#Write a statement that prompts the user for an income, looks up the tax rate
+#then prints up the income, tax rate, and tax
+
