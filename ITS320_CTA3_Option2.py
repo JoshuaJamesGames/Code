4a4192b93feb561bf0ad3937031d75abe6b64a44
Joshua James <thecoloradothompsons@gmail.com>
2022-02-28 21:25:12 -0700

    First Draft complete - will revisit before turn-in

--- ITS320_CTA3_Option2.py
+++ ITS320_CTA3_Option2.py
@@ -21,6 +21,7 @@ tax_brackets = {
 #Initialize some variables
 tax_rate = 0
 tax = 0
+tax_range = ""
 
 #Get input from the user with a prompt
 print('This program will tell you what your tax rate and taxes are based on your input.')
@@ -29,14 +30,26 @@ print('This program will tell you what your tax rate and taxes are based on your
 #Convert string type to integer
 user_income = int(input('What is your weekly income?: '))
 
-#The classic if-elif-else sequence
+#The classic if, elif..., else sequence
 if user_income < 500:
     tax_rate = tax_brackets['Less than 500']
+    tax_range = 'Less than $500'
 elif (user_income >= 500) and (user_income <= 1499):
     tax_rate = tax_brackets['500 to 1499']
+    tax_range = 'between $500 and $1499'
 elif (user_income >= 1500) and (user_income <= 2499):
     tax_rate = tax_brackets['1500 to 2499']
+    tax_range = 'between $1500 and $2499'
 else:
     tax_rate = tax_brackets['2500 or more']
+    tax_range = '$2500 or more'
 
-print(tax_rate)
+#Calculate weekly taxes
+tax = int((user_income/100)*tax_rate) 
+
+#Output useful information!
+print(f'\nYou make ${user_income} per week.')
+print(f'That is ${user_income * 52} a year, before taxes.')
+print(f'Your taxe rate is {tax_rate}%.')
+print(f'That means you will pay ${tax} per week or ${tax*52} in taxes a year.')
+print(f'After taxes, you will make ${(user_income - tax)} per week or ${(user_income - tax)*52} per year.')
