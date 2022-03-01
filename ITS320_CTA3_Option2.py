51a3c73570a99c3d3b8480d2d5104879d995b9f3
Joshua James <thecoloradothompsons@gmail.com>
2022-02-28 20:37:22 -0700

    if-elif-else structure implemented

--- ITS320_CTA3_Option2.py
+++ ITS320_CTA3_Option2.py
@@ -1,6 +1,7 @@
 #Create a program that will calculate the weekly average tax witholding for a customer
 #given the folling weekly income guidelines
-#Income less than/equal to $500: tax rate 10%
+#Income less than $500: tax rate 10%
+#Income greater than/equal to $500 and less than $1500: tax rate 15%
 #Income greater than/equal to $1500 and less than $2500: tax rate 20%
 #Income greater than/equal to $2500: tax rate 30%
 
@@ -11,11 +12,31 @@
 #Set up the income brackets and rates
 #Using integer keys and values so I can parse them later
 tax_brackets = {
-    500:10,
-    1500:20,
-    2500:30
+'Less than 500': 10,
+'500 to 1499': 15,
+'1500 to 2499': 20,
+'2500 or more': 30
 }
+
+#Initialize some variables
+tax_rate = 0
+tax = 0
 
 #Get input from the user with a prompt
 print('This program will tell you what your tax rate and taxes are based on your input.')
-user_income = input('What is your weekly income?: ')
+
+#We still haven't discussed input sanitation so I will assume perfect answers
+#Convert string type to integer
+user_income = int(input('What is your weekly income?: '))
+
+#The classic if-elif-else sequence
+if user_income < 500:
+    tax_rate = tax_brackets['Less than 500']
+elif (user_income >= 500) and (user_income <= 1499):
+    tax_rate = tax_brackets['500 to 1499']
+elif (user_income >= 1500) and (user_income <= 2499):
+    tax_rate = tax_brackets['1500 to 2499']
+else:
+    tax_rate = tax_brackets['2500 or more']
+
+print(tax_rate)
