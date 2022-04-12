b8e59524b00362c4c2558810e3eaae8911602589
Joshua Thompson <thecoloradothompsons@gmail.com>
2022-04-11 19:16:31 -0600

    bunnyCombinations() is the wrong approach

--- FooBar4_2.py
+++ FooBar4_2.py
@@ -19,6 +19,9 @@
 #The wording "lowest ID #s" indicates I will be trying all combinations of
 #bunnies
 
+#I need permutations of the bunny ID #s
+from itertools import permutations
+
 #From https://en.wikipedia.org/wiki/Bellman%E2%80%93Ford_algorithm
 def bellmanFord(graph, source):
     distances = [float('inf')] * len(graph)
@@ -32,12 +35,30 @@ def bellmanFord(graph, source):
                     distances[destination] = distances[node] + graph[node][destination]
 
     return distances
-
-
+#This was a great learning experience...but I need permutations not combinations
+def bunnyCombinations(bunny_list, combo_length):
+    if combo_length == 0:
+        return [[]]
+     
+    combo_list =[]
+    for index in range(len(bunny_list)):
+         
+        m = bunny_list[index]
+        remLst = bunny_list[index + 1:]
+         
+        for piece in bunnyCombinations(remLst, combo_length-1):
+            combo_list.append([m]+piece)
+             
+    return combo_list
 
 
 def solution(times, times_limit):
+    num_bunnies = len(times)-2
+    bunny_indexes = [x for x in range(1,num_bunnies+1)]
+
     print(bellmanFord(times, 0))
+    
+    print(list(permutations(bunny_indexes)))
     pass
 
 #Sample 1 expected answer [1,2]
@@ -57,3 +78,12 @@ print(solution([
     [1, 1, 1, 0, 1], 
     [1, 1, 1, 1, 0]
 ],3))
+
+print(solution([
+    [0, 1, 1, 1, 1, 1], 
+    [1, 0, 1, 1, 1, 1], 
+    [1, 1, 0, 1, 1, 1], 
+    [1, 1, 1, 0, 1, 1], 
+    [1, 1, 1, 1, 0, 1],
+    [1, 1, 1, 1, 1, 0]
+],4))
