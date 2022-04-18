ac60bf2e7f89ee152ab2a19cd0356b7af4728cde
Joshua James <thecoloradothompsons@gmail.com>
2022-04-18 10:28:31 -0600

    CSC320_CT1 homework

---
+++ CSC320_CT1.java
@@ -0,0 +1,34 @@
+/**
+ * CSC320_CT1
+ * Develop a program that prints a fake person with the following attributes
+ * 
+ * First Name
+ * Last Name
+ * Street
+ * City
+ * Zip Code
+ * 
+ * Each on their own line
+ */
+
+public class CSC320_CT1 {
+
+    public static void main(String[] args) {
+        //Print one fakePerson
+        printFakePerson();
+    }
+
+    //Critical Thinking 1 - Option 1 - Print a fake person 
+    public static void printFakePerson() {
+        //First Name
+        System.out.println("James");
+        //Last Name
+        System.out.println("Walden");
+        //Street Address
+        System.out.println("1775 Revolution Ave");
+        //City
+        System.out.println("Boston");
+        //Zip Code
+        System.out.println("02114");
+    }
+}
