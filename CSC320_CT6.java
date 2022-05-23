29912aee9822649e9fdb151b500c2f7bfd401a66
Joshua James <thecoloradothompsons@gmail.com>
2022-05-23 11:48:23 -0600

    CT6 Initial Setup

---
+++ CSC320_CT6.java
@@ -0,0 +1,36 @@
+/*
+Option #2 - Create a Method to Return a String Array
+
+Write program with a method that returns an array. The method should accept 
+as input a comma-delimited string with three values from a user. 
+
+The array should store each value in a different element. 
+
+Use Try..Catch error handling and print any failure messages, or print 
+success from within method if execution is successful 
+(see Chapter 6 in the text). 
+
+Call the method from the main method of the program to demonstrate its 
+functionality by looping through the array and printing the individual values.
+*/
+
+public class CSC320_CT6 {
+    public static void main(String args[]){
+        //Initialize
+        String userInput[] = new String[3];
+        //Intro
+        System.out.println("Please Input 3 Strings(Words) and I will repeat them.");
+
+        //Get Input from user
+
+        //Call method to store user's input into 3 element array
+
+        //Print out array
+
+
+    }
+
+    
+
+
+}
